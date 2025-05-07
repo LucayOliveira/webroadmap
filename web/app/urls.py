20512d74e5ffc62f4f65f78c9a584ab7ff766a4b
LucasOliver <99622185+LucayOliveira@users.noreply.github.com>
2025-05-06 22:33:52 -0300

    Criando novos várias páginas
    1- Cria as funções no View.
    2- Cria os templates html.
    3 - Criar os caminhos no urls.py

--- web/app/urls.py
+++ web/app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', homepage, name='homepage')
+    path('', Homepage, name='Homepage'),
+    path('loja/', Loja, name='Loja'),
+    path('login/', Login, name='Login'),
+    path('minhaconta/', Minhaconta, name='Minhaconta'),
+    path('carrinho/', Carrinho, name='carrinho'),
 ]
--- web/app/views.py
+++ web/app/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def homepage(request):
+def Homepage(request):
   return render(request, 'homepage.html')
+
+def Loja(request):
+  return render(request, 'loja.html')
+
+def Login(request):
+  return render(request, 'login.html')
+
+def Minhaconta(request):
+  return render(request, 'minhaconta.html')
+
+def Carrinho(request):
+  return render(request, 'carrinho.html')
