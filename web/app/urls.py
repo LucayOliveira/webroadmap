a62a652bdfe226751b26250b663be10222269ecf
LucasOliver <99622185+LucayOliveira@users.noreply.github.com>
2025-05-09 00:15:14 -0300

    criando relações entre as paginas
    {% include %}
    {% block body %}
    {% endblock %}

--- web/app/urls.py
+++ web/app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('loja/', Loja, name='Loja'),
     path('login/', Login, name='Login'),
     path('minhaconta/', Minhaconta, name='Minhaconta'),
-    path('carrinho/', Carrinho, name='carrinho'),
+    path('carrinho/', Carrinho, name='Carrinho'),
 ]
